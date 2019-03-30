0090f3427f4f1546e75f1b62edbd37a5eb05c490
Steve Belovarich <steveblue@gmail.com>
2019-03-29 21:37:34 -0700

    fix build

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -1,5 +1,5 @@
 # builds npm package
+node_modules/.bin/tsc -p tsconfig.json
 cp package.json dist/package.json
 cp builders.json dist/builders.json
 cp src/rollup/schema.json dist/src/rollup/schema.json
-node_modules/.bin/tsc -p tsconfig.json
