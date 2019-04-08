71db3c814c243482f8b8f8489d71218db9b74838
Steve Belovarich <steveblue@gmail.com>
2019-04-08 16:52:18 -0700

    fix issue with build script

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -3,3 +3,4 @@ node_modules/.bin/tsc -p tsconfig.json
 cp package.json dist/package.json
 cp builders.json dist/builders.json
 cp src/rollup/schema.json dist/src/rollup/schema.json
+cp src/closure/schema.json dist/src/closure/schema.json
